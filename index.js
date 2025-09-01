c84ac48b1502e8c7cbc04c7de5a5bf438113321e
Elegbede Alvin Olamide <elegbedealvin2022@gmail.com>
2025-09-01 10:40:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+let me = document.getElementById("dad");
+me.style.display = "flex";
+me.style.justifyContent = "center";
+me.style.alignContent = "center";
+me.style.height = "100vh";
+me.style.backgroundColor = "lightblue";
+me.style.textAlign = "center";
+me.style.fontFamily = "new-serif"
+
+let as = document.getElementById("over");
+as.style.width = "250px";
+as.style.height = "200px";
+as.style.backgroundColor = "gray";
+as.style.marginTop = "120px";
+as.style.padding = "25px 17px";
+as.style.border = "2px solid purple";
+as.style.borderRadius = "13px";
+
+let yes = document.getElementById("mad");
+yes.style.color = "purple";
+yes.style.fontWeight = "bolder";
+yes.style.fontSize = "2rem";
+
+let pick = document.getElementById("idiot1");
+pick.style.marginTop = "20px";
+pick.style.padding = "7px";
+
+let at = document.getElementById("idiot2");
+at.style.marginBottom = "5px";
+at.style.padding = "3px";
+
+let to = document.getElementById("email");
+to.style.borderRadius = "4px";
+to.style.padding = "3px";
+to.style.border = "2px solid purple";
+
+let tom = document.getElementById("password");
+tom.style.borderRadius = "4px";
+tom.style.padding = "3px";
+tom.style.border = "2px solid purple";
+
+let button = document.getElementById("sweet");
+button.style.color = "white";
+button.style.backgroundColor = "purple";
+button.style.fontFamily = "serif";
+button.style.padding = "6px";
+button.style.width = "80px";
+button.style.marginBottom = "10px";
+button.style.borderRadius = "6px";
